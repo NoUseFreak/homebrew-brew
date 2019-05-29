9aaf867dec395f7ec28352d2f7c451767265493a
Dries De Peuter <dries@nousefreak.be>
2019-05-29 19:52:45 +0200

    Add sawsh

---
+++ Formula/sawsh.rb
@@ -0,0 +1,14 @@
+class Sawsh < Formula
+    desc "SSH wrapper for aws to make your life easier"
+    homepage "https://github.com/NoUseFreak/sawsh"
+    url "https://github.com/NoUseFreak/sawsh/releases/download/0.2.3/darwin_amd64.tar.gz"
+    version "0.3.4"
+  
+    def install
+      bin.install "sawsh"
+    end
+  
+    test do
+      system "#{bin}/sawsh", "-h"
+    end
+  end
