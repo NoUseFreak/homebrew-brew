db3436435e23b468b82bf0e0253e768faf288630
Dries De Peuter <dries@nousefreak.be>
2019-09-27 23:38:11 +0200

    Upgrade gitteams to 0.1.10

--- Formula/gitteams.rb
+++ Formula/gitteams.rb
@@ -1,1 +1,18 @@
-CmNsYXNzIEdpdHRlYW1zIDwgRm9ybXVsYQogIGRlc2MgIkdlbmVyaWMgcmVsZWFzZSBoZWxwZXIiCiAgaG9tZXBhZ2UgImh0dHBzOi8vZ2l0aHViLmNvbS9Ob1VzZUZyZWFrL3JlbGVhc2UiCgogIHZlcnNpb24gIjAuMS4xMCIKICB1cmwgImh0dHBzOi8vZ2l0aHViLmNvbS9Ob1VzZUZyZWFrL2dpdHRlYW1zL3JlbGVhc2VzL2Rvd25sb2FkLzAuMS4xMC9kYXJ3aW5fYW1kNjQuemlwIgogIHNoYTI1NiAiZmJiYzcyM2RhMGY1NWNhZDg2NTFlNjI2ZDBmYjdjMjAzMmY5NDdiMjVhNmUyMGYzNTA0NDFiZjZjZTYxOWRhOCIKCiAgZGVmIGluc3RhbGwKICAgIGJpbi5pbnN0YWxsICJnaXR0ZWFtcyIKICBlbmQKCgogIHRlc3QgZG8KICAgIHN5c3RlbSAiI3tiaW59L2dpdHRlYW1zIC1oIgogIGVuZAplbmQK
+
+class Gitteams < Formula
+  desc "Generic release helper"
+  homepage "https://github.com/NoUseFreak/release"
+
+  version "0.1.10"
+  url "https://github.com/NoUseFreak/gitteams/releases/download/0.1.10/darwin_amd64.zip"
+  sha256 "fbbc723da0f55cad8651e626d0fb7c2032f947b25a6e20f350441bf6ce619da8"
+
+  def install
+    bin.install "gitteams"
+  end
+
+
+  test do
+    system "#{bin}/gitteams -h"
+  end
+end
