317436be0dcc1cbc74ad18368210c420500494c6
Dries De Peuter <dries@nousefreak.be>
2019-10-26 09:55:47 +0200

    Upgrade letitgo to 0.8.0

--- Formula/letitgo.rb
+++ Formula/letitgo.rb
@@ -3,9 +3,9 @@ class Letitgo < Formula
   desc "LetItGo automates releases."
   homepage "https://github.com/NoUseFreak/letitgo"
 
-  version "0.7.0"
-  url "https://github.com/NoUseFreak/letitgo/releases/download/0.7.0/darwin_amd64.zip"
-  sha256 "2e5c9d631ccfcd1c8cbec4ae4cced5066be78780ec14b360820446d92c6a5191"
+  version "0.8.0"
+  url "https://github.com/NoUseFreak/letitgo/releases/download/0.8.0/darwin_amd64.zip"
+  sha256 "68ae539c556a9a8ace2f38e5a95fcba9dc1189d21d2a30188e261c0ecdafd3fb"
 
   def install
     bin.install "letitgo"
