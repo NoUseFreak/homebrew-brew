0ffe682b0e90aa21389e0564b1cd4c24a6802506
Dries De Peuter <dries@nousefreak.be>
2019-11-04 20:44:20 +0100

    Upgrade sdf to 0.2.0

--- Formula/sdf.rb
+++ Formula/sdf.rb
@@ -3,9 +3,9 @@ class Sdf < Formula
   desc "sdf for the lazy developer"
   homepage "https://github.com/NoUseFreak/sdf"
 
-  version "0.1.4"
-  url "https://github.com/NoUseFreak/sdf/releases/download/0.1.4/darwin_amd64.zip"
-  sha256 "92e7fd77e6bbf51d97b4fff7ebbc5f6dca0b55fb18c8152c4b2bbe218f2a5ef1"
+  version "0.2.0"
+  url "https://github.com/NoUseFreak/sdf/releases/download/0.2.0/darwin_amd64.zip"
+  sha256 "4e6115e70e38fe4f10130810f14e2baa726d0c615b0b83564d60ce0741a5d142"
 
   def install
     bin.install "sdf"
